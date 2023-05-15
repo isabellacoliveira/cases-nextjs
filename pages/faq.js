2d26840020b7f2ea021f71ce91842816761bbd53
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-05-15 17:31:41 -0300

    adicionando as primeiras rotas da aplicação

---
+++ pages/faq.js
@@ -0,0 +1,6 @@
+export default function faqPage(){
+    return <div>
+    <h1>Alura Cases - Páginas de Perguntas</h1>
+    <a href="/">Ir para a HOME </a>
+</div>
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function homePage(){
+    return (
+        <div>
+            <h1>Alura Cases - Páginas de Perguntas</h1>
+            <a href="/faq">Ir para o FAQ </a>
+        </div>
+    )
+}
